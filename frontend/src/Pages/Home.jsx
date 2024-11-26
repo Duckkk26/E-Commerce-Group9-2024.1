3aa5f1d487e380f2a0f139fab2b4d2a7b6bc5db7
vuminhduc1702 <vuminhduc1702@gmail.com>
2024-11-26 15:19:06 +0700

    homepage - new collection

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Banner from "../Components/Banner/Banner";
 
 import "./CSS/Home.css";
+import NewCollections from "../Component/NewCollections/NewCollections";
 
 function Home() {
   return (
     <div className="home">
       <Banner />
-      
+      <NewCollections />
     </div>
   );
 }
