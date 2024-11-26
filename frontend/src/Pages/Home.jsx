0f38634f225a7e7f59101469b44e6cfa29b37306
vuminhduc1702 <vuminhduc1702@gmail.com>
2024-11-26 15:21:03 +0700

    homepage - popular

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -3,12 +3,17 @@ import Banner from "../Components/Banner/Banner";
 
 import "./CSS/Home.css";
 import NewCollections from "../Component/NewCollections/NewCollections";
+import Popular from "../Component/Popular/Popular";
 
 function Home() {
   return (
     <div className="home">
       <Banner />
       <NewCollections />
+      <Popular category={"Mobile"} />
+      <Popular category={"Laptop"} />
+      <Popular category={"PersonalComputer"} />
+      <Popular category={"Tablet"} />
     </div>
   );
 }
