2b4ba29693b1d1af0431e41a6978467e0b278b8f
Saphyiera <nguyenduc281107@gmail.com>
2024-12-01 19:56:14 +0700

    add cart router

--- backend/src/api/routes/index.js
+++ backend/src/api/routes/index.js
@@ -1,9 +1,11 @@
 import { router as productRouter } from './product.js'
 import { router as brandRouter } from './brand.js';
+import { router as cartRouter } from './cart.js';
 
 function route(app) {
     app.use('/product', productRouter);
     app.use('/brand', brandRouter);
+    app.use('/cart', cartRouter)
 }
 
 export { route };
