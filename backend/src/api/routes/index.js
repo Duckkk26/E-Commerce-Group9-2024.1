19124bbe8758fa87554c4a2aec0d7da7ae121504
Nguyễn Trọng Đức <trongduc2003ht@gmail.com>
2024-12-04 08:45:20 +0700

    feat: pay router

--- backend/src/api/routes/index.js
+++ backend/src/api/routes/index.js
@@ -1,7 +1,11 @@
+import { router as orderRouter } from "./order";
 import { router as userRouter } from "./user";
+import { router as payRouter } from './pay'
 
 function route(app) {
     app.use('/user', userRouter)
+    app.use('/order', orderRouter)
+    app.use('/pay', payRouter)
 }
 
 export { route }
