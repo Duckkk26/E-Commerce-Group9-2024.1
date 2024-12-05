48e79f0d81e32c352c8eff319b9fc56301714350
vuminhduc1702 <vuminhduc1702@gmail.com>
2024-12-06 00:47:52 +0700

    add product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from "./Component/Header/Header";
 import "./App.css";
 import Home from "./Pages/Home";
 import ShopCategory from "./Pages/ShopCategory";
+import Product from "./Pages/Product";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
               element={<ShopCategory category="Mobile" />}
             />
           </Route>
+          <Route path="/product" element={<Product />} />
         </Routes>
       </BrowserRouter>
     </>
