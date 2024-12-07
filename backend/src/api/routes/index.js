8db38a26d37d9c1c4bf4b159321ad5e2041f9683
Nguyễn Trọng Đức <trongduc2003ht@gmail.com>
2024-12-07 11:50:24 +0700

    feat: address api

--- backend/src/api/routes/index.js
+++ backend/src/api/routes/index.js
@@ -1,11 +1,13 @@
 import { router as orderRouter } from "./order";
 import { router as userRouter } from "./user";
 import { router as payRouter } from './pay'
+import { router as addressRouter } from "./address";
 
 function route(app) {
     app.use('/user', userRouter)
     app.use('/order', orderRouter)
     app.use('/pay', payRouter)
+    app.use('/address', addressRouter)
 }
 
 export { route }
